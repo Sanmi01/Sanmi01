c7142fc9a643de20e7e152d096587f0f66d5113c
Sanmi01 <supersanmi01@gmail.com>
2020-12-10 01:37:20 +0100

    more animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
 var tl = gsap.timeline();
 
-tl.from('.project', {
-    x: '-50%',
-    opacity: 0
+tl.from('.left-project', {
+    x: '-70%',
+    opacity: 0,
+    duration: 2,
+    ease: Power2.easeOut
 })
+
+tl.from('.right-project', {
+    x: '70%',
+    opacity: 0,
+    duration: 2,
+    ease: Power2.easeOut
+}, "-=2")
